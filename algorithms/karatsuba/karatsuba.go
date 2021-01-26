f7691d48259dad48fadd27755cc4954bf10dc6ef
dingwenchao <dingwenchao@shuame.com>
2021-01-26 05:30:17 +0000

    update

--- algorithms/karatsuba/karatsuba.go
+++ algorithms/karatsuba/karatsuba.go
@@ -1,11 +1,14 @@
 package karatsuba
 
+import "fmt"
+
 func karatsuba(a int64, b int64) int64 {
 	return 232
 }
 
 // Karatsuba -
 func Karatsuba(a int64, b int64) int64 {
+	fmt.Printf("saasd%d %d\n", a, b)
 	// aStr := String(a)
 	// bStr := String(b)
 	// len(astr)
