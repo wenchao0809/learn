eb3e80e5e03381f38336bce5d4582853dbf23a02
et <970746082@qq.com>
2021-04-21 00:48:51 +0800

    update

--- leetcode-javascript/javascript/28.js
+++ leetcode-javascript/javascript/28.js
@@ -2,7 +2,7 @@
  * 
  * @param {string} p 
  */
-function buildNext(p) {
+ function buildNext(p) {
   const m = p.length
   const N = []
   let t = N[0] = -1
@@ -11,7 +11,7 @@ function buildNext(p) {
     if (0 > t || p[j] === p[t]) {
       j++
       t++
-      // 假如t是原字符串 根据已知信息如果p[j] !== t[i] 如果p[j] === p[t]则p[t]必然失配t[i]所以这次比较可以优化
+      // 根据已知信息如果p[j] !== t[i] 如果p[j] === p[t]则p[t]必然失配t[i]所以这次比较可以优化
       N[j] = p[j] !== p[t] ? t : N[t]
     } else {
       t = N[t]
@@ -40,18 +40,6 @@ function match(t, p) {
   }
   return j === m ? i - j : -1
 }
-
-function indexOf(s, fromIndex = 0) {
-  fromIndex = 0 > fromIndex  
-    ? this.length + fromIndex > 0
-      ? this.length + fromIndex
-      : 0
-    : fromIndex
-  console.log(fromIndex)
-  const i = match(this.slice(fromIndex), s)
-  return i > 0 ? fromIndex + i : i
-}
-
 /**
  * @param {string} haystack
  * @param {string} needle
@@ -61,4 +49,4 @@ var strStr = function(haystack, needle) {
   return match(haystack, needle)
 };
 
-console.log(strStr('hello', ''))
+console.log(strStr('hello', 'll'))
